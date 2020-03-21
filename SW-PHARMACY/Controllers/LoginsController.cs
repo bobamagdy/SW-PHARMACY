0c4ef36e4223d44411b30e24677a821966b25d30
bobamagdy <bobamagdy1333@gmail.com>
2020-03-21 05:22:35 +0200

    controllers are added

---
+++ SW-PHARMACY/Controllers/LoginsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SW_PHARMACY.Models;
+
+namespace SW_PHARMACY.Controllers
+{
+    public class LoginsController : Controller
+    {
+        private DBprojectEntities db = new DBprojectEntities();
+
+        // GET: Logins
+        public ActionResult Index()
+        {
+            var logins = db.Logins.Include(l => l.Role);
+            return View(logins.ToList());
+        }
+
+        // GET: Logins/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // GET: Logins/Create
+        public ActionResult Create()
+        {
+            ViewBag.Role_Id = new SelectList(db.Roles, "Role_Id", "RoleName");
+            return View();
+        }
+
+        // POST: Logins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Login_Id,Email,Password,Isdeleted,CreatedOn,Role_Id")] Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Logins.Add(login);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Role_Id = new SelectList(db.Roles, "Role_Id", "RoleName", login.Role_Id);
+            return View(login);
+        }
+
+        // GET: Logins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Role_Id = new SelectList(db.Roles, "Role_Id", "RoleName", login.Role_Id);
+            return View(login);
+        }
+
+        // POST: Logins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Login_Id,Email,Password,Isdeleted,CreatedOn,Role_Id")] Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(login).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Role_Id = new SelectList(db.Roles, "Role_Id", "RoleName", login.Role_Id);
+            return View(login);
+        }
+
+        // GET: Logins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // POST: Logins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Login login = db.Logins.Find(id);
+            db.Logins.Remove(login);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
